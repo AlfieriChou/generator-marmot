68f06402297f14e6818baf74ced805dbae1a5d90
AlfieriChou <alfierichou@gmail.com>
2019-11-19 21:59:27 +0800

    test: package.json

--- __tests__/app.js
+++ __tests__/app.js
@@ -5,12 +5,16 @@ const helpers = require("yeoman-test");
 
 describe("generator-marmot:app", () => {
   beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, "../generators/app"))
-      .withPrompts({ someAnswer: true });
+    return helpers.run(path.join(__dirname, "../generators/app")).withPrompts({
+      name: "marmot",
+      verson: "0.0.1",
+      description: "test",
+      author: "AlfieriChou",
+      email: "alfierichou@gmail.com"
+    });
   });
 
   it("creates files", () => {
-    assert.file(["dummyfile.txt"]);
+    assert.file(["marmot/package.json"]);
   });
 });
